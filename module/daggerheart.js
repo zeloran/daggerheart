af8abc859add8f25e1aff98a94b567e6b19b88ca
Pablo Queirolo <>
2025-06-26 18:16:54 -0400

    no message

--- module/daggerheart.js
+++ module/daggerheart.js
@@ -1,4 +1,9 @@
+// ES module entrypoint for Daggerheart system (Foundry V13+)
+
+// Actor sheet
 import { DaggerheartActorSheet } from "./actor/sheets/character-sheet.js";
+
+// Item sheets
 import { DaggerheartSpellSheet } from "./item/sheets/spell-sheet.js";
 import { DaggerheartSubclassSheet } from "./item/sheets/subclass-sheet.js";
 import { DaggerheartAbilitySheet } from "./item/sheets/ability-sheet.js";
@@ -11,13 +16,19 @@ import { DaggerheartWeaponSheet } from "./item/sheets/weapon-sheet.js";
 Hooks.once("init", function() {
     console.log("Daggerheart | Initializing system");
 
-    Actors.registerSheet("daggerheart", DaggerheartActorSheet, { makeDefault: true });
-    Items.registerSheet("daggerheart", DaggerheartSpellSheet, { types: ["spell"], makeDefault: true });
-    Items.registerSheet("daggerheart", DaggerheartSubclassSheet, { types: ["subclass"], makeDefault: true });
-    Items.registerSheet("daggerheart", DaggerheartAbilitySheet, { types: ["ability"], makeDefault: true });
-    Items.registerSheet("daggerheart", DaggerheartAncestrySheet, { types: ["ancestry"], makeDefault: true });
-    Items.registerSheet("daggerheart", DaggerheartCommunitySheet, { types: ["community"], makeDefault: true });
-    Items.registerSheet("daggerheart", DaggerheartItemSheet, { types: ["item"], makeDefault: true });
-    Items.registerSheet("daggerheart", DaggerheartArmorSheet, { types: ["armor"], makeDefault: true });
-    Items.registerSheet("daggerheart", DaggerheartWeaponSheet, { types: ["weapon"], makeDefault: true });
+    // Register custom actor sheets
+    Actors.registerSheet("daggerheart", DaggerheartActorSheet, {
+        types: ["character", "npc"],
+        makeDefault: true
+    });
+
+    // Register custom item sheets (one for each type)
+    Items.registerSheet("daggerheart", DaggerheartSpellSheet,      { types: ["spell"],       makeDefault: true });
+    Items.registerSheet("daggerheart", DaggerheartSubclassSheet,   { types: ["subclass"],    makeDefault: true });
+    Items.registerSheet("daggerheart", DaggerheartAbilitySheet,    { types: ["ability"],     makeDefault: true });
+    Items.registerSheet("daggerheart", DaggerheartAncestrySheet,   { types: ["ancestry"],    makeDefault: true });
+    Items.registerSheet("daggerheart", DaggerheartCommunitySheet,  { types: ["community"],   makeDefault: true });
+    Items.registerSheet("daggerheart", DaggerheartItemSheet,       { types: ["item"],        makeDefault: true });
+    Items.registerSheet("daggerheart", DaggerheartArmorSheet,      { types: ["armor"],       makeDefault: true });
+    Items.registerSheet("daggerheart", DaggerheartWeaponSheet,     { types: ["weapon"],      makeDefault: true });
 });
